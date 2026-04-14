9077a82399f5c5cebff654e9b18c1bdbe6330e38
ahsan <ahsanramadan07@gmail.com>
2026-04-14 09:02:49 +0700

    all done

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -13,18 +13,21 @@
     <h1>Popular Movie</h1>
     <div class="row">
         @foreach ($movies as $movie)
-            <div class="col-lg-6">
-                <div class="card mb-3" style="max-width: 540px;">
-                    <div class="row g-0">
+            <!-- Perbaikan Grid -->
+            <div class="col-lg-6 mb-4 d-flex align-items-stretch">
+                <div class="card w-100 h-100" style="max-width: 540px;">
+                    <div class="row g-0 h-100">
                         <div class="col-md-4">
-                            <img src="{{ asset('storage/' . $movie['foto_sampul']) }}" class="img-fluid rounded-start"
+                            <img src="{{ asset('storage/' . $movie['foto_sampul']) }}" class="img-fluid rounded-start h-100 w-100" style="object-fit: cover;"
                                 alt="...">
                         </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
+                        <div class="col-md-8 d-flex flex-column">
+                            <div class="card-body d-flex flex-column h-100">
                                 <h5 class="card-title">{{ $movie['judul'] }}</h5>
-                                <p class="card-text">{{ $movie['sinopsis'] }}</p>
-                                <a href="/movie/{{ $movie['id'] }}" class="btn btn-success">Lihat Selanjutnya</a>
+                                <p class="card-text flex-grow-1">{{ Str::limit($movie['sinopsis'], 120) }}</p>
+                                <div class="mt-auto">
+                                    <a href="/movie/{{ $movie['id'] }}" class="btn btn-success">Lihat Selanjutnya</a>
+                                </div>
                             </div>
                         </div>
                     </div>
